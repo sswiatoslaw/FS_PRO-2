0c5fa8397fa1847852f54a22d5f714ad50be5684
sswiatoslaw <sviatoslav.sodel@creditkasa.ua>
2022-07-28 18:00:41 +0300

    add 11 lesson

--- #11-Constructor-site/app/js/script.js
+++ #11-Constructor-site/app/js/script.js
@@ -1,5 +1,7 @@
 const root = document.querySelector('.root');
 const tagList = ['div', 'header', 'section', 'footer', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'ul', 'li'];
+let currentIndex = 0;
+
 window.addEventListener('change', () => {
     const selectList = document.querySelectorAll('select.tag-list');
 
@@ -23,79 +25,106 @@ const renderTag = (object) => {
     const currentRenderedTag = document.createElement(object.tag);
     currentRenderedTag.className = object.className;
     currentRenderedTag.id = object.id;
-    currentRenderedTag.innerHTML = object.value;
+    if(typeof object.value === 'object') {
+        currentRenderedTag.append(renderTagObject(object.value));
+    } else {
+        currentRenderedTag.innerHTML = object.value;
+    }
     return currentRenderedTag;
 }
 
+const renderTagObject = (objectValue) => renderTag(objectValue);
 
-const createTag = (tagName) => {
-    const tagNameArray = ['tag', 'className', 'id', 'value'];
+const createTag = (tagName, tagIndex) => {
+    const tagNameArray = ['tag', 'className', 'id', 'value', 'value-select'];
     let tagObject = {};
     tagNameArray.forEach((tag) => {
-        const currentValue = document.querySelector('#' + tagName + '-' + tag);
-        tagObject = {...tagObject, [tag]: currentValue.value }
+        const currentValue = document.querySelector('#' + tagName + '-' + tag + '-' + tagIndex);
+        if(currentValue) {
+            if(tag === 'value-select' && currentValue.value === 'tag') {
+                tagObject = {...tagObject, value: createTagObject('tag', currentIndex) }
+            } else {
+                console.log(currentValue);
+                tagObject = {...tagObject, [tag]: currentValue.value }
+            }
+        }
     })
     return tagObject
 }
+
+const createTagObject = (tagName, tagIndex) => createTag(tagName, tagIndex);
 
 const createHeader = (object) => {
     return { header: object }
 }
 
 const renderSite = (siteObject) => {
+    console.log(siteObject);
     root.innerHTML = '';
         Object.keys(siteObject).forEach((key => {
             root.append(renderTag(siteObject[key]));
         }))
 }
 
-const newTag = () => {
+const newTag = (index) => {
     const tagWrapper = document.createElement('div')
-    tagWrapper.className = 'tag-wrapper';
+    tagWrapper.className = 'tag__wrapper';
     const tagSelect = document.createElement('select');
     tagSelect.className = 'tag-list'
+    tagSelect.id = 'tag-tag-' + index
     const tagInputClassName = document.createElement('input');
-    tagInputClassName.id = 'tag-className';
+    tagInputClassName.id = 'tag-className-' + index;
+    tagInputClassName.placeholder = 'element className';
     const tagInputId = document.createElement('input');
-    tagInputId.id = 'tag-id';
+    tagInputId.id = 'tag-id-' + index;
+    tagInputId.placeholder = 'element id';
     const tagSelectValue = document.createElement('select');
-    tagSelectValue.id = 'tag-value-select';
+    tagSelectValue.id = 'tag-value-select-' + index;
     ['Text', 'Tag'].forEach((option) => {
         const tagOption = document.createElement('option');
         tagOption.value = option.toLowerCase();
         tagOption.textContent = option;
         tagSelectValue.append(tagOption);
     })
+    const tagInputValue = document.createElement('input');
+    tagInputValue.id = 'tag-value-' + index;
+
     tagWrapper.append(tagSelect);
     tagWrapper.append(tagInputClassName);
     tagWrapper.append(tagInputId);
     tagWrapper.append(tagSelectValue);
+    tagWrapper.append(tagInputValue);
     return tagWrapper
 }
 
-const newValue = (param) => {
+const newValue = (param, index) => {
     const valueWrapper = document.querySelector('.header-value-wrapper');
     valueWrapper.innerHTML = '';
     if(param === 'text') {
         const valueInput = document.createElement('input');
-        valueInput.id = 'header-value'
+        valueInput.id = 'header-value-0'
         valueInput.placeholder = 'Enter value for header'
         valueWrapper.append(valueInput);
     } else if (param === 'tag') {
-        valueWrapper.append(newTag());
+        valueWrapper.append(newTag(index));
     }
 }
 
-const valueSelect = document.querySelector('#header-value-select');
+const valueSelect = document.querySelector('#header-value-select-0');
+
+
+
 valueSelect.addEventListener('change', (event) => {
-    console.log(event.target.value);
-    newValue(event.target.value);
+    currentIndex++;
+    console.log(currentIndex);
+    newValue(event.target.value, currentIndex);
 })
 
 
 const createHeaderButton = document.querySelector('#create-header-button');
+
 createHeaderButton.addEventListener('click', (element) => {
-    const siteObject = createHeader(createTag('header'));
+    const siteObject = createHeader(createTag('header', 0));
     localStorage.setItem('renderSite', JSON.stringify(siteObject));
     renderSite(siteObject);
 });
@@ -117,4 +146,11 @@ const checkLocalStorage = () => {
         alert('Welcome to constructor!');
     }
 }
-window.onload = checkLocalStorage();
+
+const startConstructor = () => {
+    checkLocalStorage();
+    newValue(valueSelect.value);
+}
+window.onload = startConstructor()  ;
+
+
